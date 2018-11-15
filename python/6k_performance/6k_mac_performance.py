a06b80c9c81b5b3d6c9194649b2b00c968d0fff8
EasyMunan <953585966@qq.com>
2018-11-15 23:01:23 +0800

    commit

--- python/6k_performance/6k_mac_performance.py
+++ python/6k_performance/6k_mac_performance.py
@@ -3,8 +3,8 @@ import sys
 
 fp = open('./6k_mac_performance.txt', 'w')
 for i in range(1, 17):
-    fp.write('mac-address static 00:00:00:00:00:%d interface %d vlan 1' % (i+10, i+32))
+    fp.write('mac-address static 00:00:00:00:00:%d interface %d vlan 1\n' % (i+10, i+32))
 
 for i,j,l in range((17, 37),(49, 55),(1, 5)):
-    fp.write('mac-address static 00:00:00:00:00:%d interface %d-%d vlan 1' % (i+10, j, l))
+    fp.write('mac-address static 00:00:00:00:00:%d interface %d-%d vlan 1\n' % (i+10, j, l))
 fp.close()
